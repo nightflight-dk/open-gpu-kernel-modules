870026c0a6b532f76f4347ebc6bb313b35a0d3c4
Damian Kowalczyk <nightflight-dk@users.noreply.github.com>
2024-07-17 17:19:10 -0700

    550.90.07 p2p dk (#1)
    * need to replace this file as well (#11)
    
    * 550.67
    
    * 550.76
    
    * 550.78
    
    * 550.90.07
    
    * feat: build workflow
    
    * feat: path
    
    * feat: upload
    
    * fix: -R
    
    * feat: deterministic name
    
    * feat: create release
    
    * fix: token
    
    * fix: permissions
    
    * fix: need to copy this file over as well
    
    * install.sh compiler choice
    
    * signing for secure-boot with preexisting keys
    
    * readme
    
    * minor
    
    ---------
    
    Co-authored-by: wozeparrot <wozeparrot@gmail.com>
    Co-authored-by: Bernhard Stoeckner <bstoeckner@nvidia.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo make clean
 sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
 set -e
-make modules -j$(nproc)
-sudo make modules_install -j$(nproc)
+CC=gcc-12 CXX=g++ sudo make modules -j$(nproc)
+CC=gcc-12 CXX=g++ sudo make modules_install -j$(nproc)
 sudo depmod
 nvidia-smi
