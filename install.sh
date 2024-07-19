6a77e237cf16f3f3566194b9575852de4941209e
Damian Kowalczyk <dakowalc@microsoft.com>
2024-07-19 13:14:13 -0700

    inst

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 sudo make clean
 sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
 set -e
-CC=gcc-12 CXX=g++ sudo make modules -j$(nproc)
-CC=gcc-12 CXX=g++ sudo make modules_install -j$(nproc)
+
+CC=gcc-12 CXX=g++ sudo make clean
+CC=gcc-12 CXX=g++ make modules -j$(nproc)
+CC=gcc-12 CXX=g++ sudo checkinstall make modules_install -j$(nproc)
+
 sudo depmod
 nvidia-smi
